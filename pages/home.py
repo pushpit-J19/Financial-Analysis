f70a0144eaece9b6a06138f8cb3338bdafec7ecd
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2024-10-17 22:29:21 +0530

    Create home.py

---
+++ pages/home.py
@@ -0,0 +1,8 @@
+import dash
+from dash import html
+
+dash.register_page(__name__, path='/')
+
+layout = html.Div([
+    html.Div('This site provides interactive tools to valuate and analyze stocks through Reverse DCF model. Check the navigation bar for more.'),
+])
