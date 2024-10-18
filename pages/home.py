168de6aca488bcac2dc34c269f5b2e7cda713cd8
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2024-10-18 18:06:50 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html
 
-dash.register_page(__name__, path=['/', '/home'], name = "Home")
+dash.register_page(__name__, path='/home', name = "Home")
 
 layout = html.Div([
     html.Div('This site provides interactive tools to valuate and analyze stocks through Reverse DCF model. Check the navigation bar for more.'),
