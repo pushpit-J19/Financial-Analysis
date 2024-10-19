9f52199aa4890c8e0c3edee8cd574825e640e6e4
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2024-10-19 14:17:33 +0530

    Minor css

--- pages/home.py
+++ pages/home.py
@@ -6,8 +6,8 @@ import dash_bootstrap_components as dbc
 dash.register_page(__name__, path='/', name = "Home", external_stylesheets=[dbc.themes.BOOTSTRAP, 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css'])
 
 layout = html.Div(
-    style = {"padding":"5%"}, 
-    className = "container", 
+    # style = {"padding":"5%"}, 
+    className = "px-5 py-2", 
     children = [
         html.Div('This site provides interactive tools to valuate and analyze stocks through Reverse DCF model. Check the navigation bar for more.'),
     
